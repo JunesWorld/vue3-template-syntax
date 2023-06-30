7895e0d51c5cf0e46de1e9abc9694dffbe141756
June <sungj1207@gmail.com>
2023-06-30 17:12:52 +0900

    클래스와 스타일 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,28 @@
 <template>
-  <h1 @click="changeMessage">
-    {{ msg }}
+  <h1
+    :style="[fontStyle, backgroundStyle]"
+    @click="changeStyle">
+    Hello?!
   </h1>
-  <h1>{{ reversedMessage }}</h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      msg: 'Hello?'
-    }
-  },
-  computed: {
-    reversedMessage() {
-      return this.msg.split('').reverse().join('')
-    }
-  },
-  // 변경 사항 감시하는 Option
-  // console에 출력
-  watch: {
-    msg(newValue) {
-      console.log('msg:', newValue)
-    },
-    reversedMessage(newValue) {
-      console.log('reversedMessage: ', newValue)
+      fontStyle: {
+        color: 'orange',
+        fontSize: '30px'
+      },
+      backgroundStyle: {
+        backgroundColor: 'black'
+      }
     }
   },
   methods: {
-    changeMessage() {
-      this.msg = 'Good!'
+    changeStyle() {
+      this.fontStyle.color = 'red'
+      this.fontStyle.fontSize = '50px'
     }
   }
 }

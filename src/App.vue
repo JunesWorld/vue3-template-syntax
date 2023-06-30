37e3c2bb2e3d8dd4229ca42dd6e856fa3fe810b6
June <sungj1207@gmail.com>
2023-06-30 15:32:18 +0900

    Watch

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
 <template>
-  <!-- Methods
-  <h1>{{ reverseMessage() }}</h1>
-  <h1>{{ reverseMessage() }}</h1>
-  <h1>{{ reverseMessage() }}</h1>
-  <h1>{{ reverseMessage() }}</h1> -->
-
-  <!-- 
-    computed : 계산된 데이터(캐싱) 
-    - 첫번째에서 계산한 값 두번째부터 그대로 반복 사용
-    - 데이터 최적화
-  -->
-  <button @click="add">
-    ADD
-  </button>
-  <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
+  <h1 @click="changeMessage">
+    {{ msg }}
+  </h1>
   <h1>{{ reversedMessage }}</h1>
 </template>
 
@@ -23,33 +9,27 @@
 export default {
   data() {
     return {
-      // Getter
-      // Setter : 값을 지정하는 용도
-      msg: 'Hello Computed!'
-    }  
+      msg: 'Hello?'
+    }
   },
-  // ReadOnly : 읽기전용
   computed: {
-    // Getter : 값을 얻어오는 용도
-    // reversedMessage() {
-    //   return this.msg.split('').reverse().join('')
-    // }
-    // Getter, Setter
-    reversedMessage: {
-      get() {
-        return this.msg.split('').reverse().join('')
-      },
-      // set(들어오는 값)
-      // reversedMessage 값을 할당할때만 동작
-      set(newValue) {
-        // console.log(newValue)로 값을 확인하고 작업!
-        this.msg = newValue
-      }
+    reversedMessage() {
+      return this.msg.split('').reverse().join('')
+    }
+  },
+  // 변경 사항 감시하는 Option
+  // console에 출력
+  watch: {
+    msg(newValue) {
+      console.log('msg:', newValue)
+    },
+    reversedMessage(newValue) {
+      console.log('reversedMessage: ', newValue)
     }
   },
   methods: {
-    add() {
-      this.reversedMessage += '!?'
+    changeMessage() {
+      this.msg = 'Good!'
     }
   }
 }

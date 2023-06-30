304a1b6e6cd74e6c7f73d9efc153b9cdaf5bdd8e
June <sungj1207@gmail.com>
2023-06-30 15:06:33 +0900

    Computed 캐싱

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
-  <Fruits />
+  <!-- Methods
+  <h1>{{ reverseMessage() }}</h1>
+  <h1>{{ reverseMessage() }}</h1>
+  <h1>{{ reverseMessage() }}</h1>
+  <h1>{{ reverseMessage() }}</h1> -->
+
+  <!-- 
+    computed : 계산된 데이터(캐싱) 
+    - 첫번째에서 계산한 값 두번째부터 그대로 반복 사용
+    - 데이터 최적화
+  -->
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
 </template>
 
 <script>
-import Fruits from '~/componets/Fruits'
-
 export default {
-  components: {
-    Fruits
-  }  
+  data() {
+    return {
+      msg: 'Hello Computed!'
+    }  
+  },
+  computed: {
+    reversedMessage() {
+      return this.msg.split('').reverse().join('')
+    }
+  },
+  methods: {
+    reverseMessage() {
+      return this.msg.split('').reverse().join('')
+    }
+  }
 }
 </script>

89e8e086359286c484eaa482c0ef9c7165ea31bc
June <sungj1207@gmail.com>
2023-06-30 15:20:42 +0900

    Getter, Setter

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@
     - 첫번째에서 계산한 값 두번째부터 그대로 반복 사용
     - 데이터 최적화
   -->
+  <button @click="add">
+    ADD
+  </button>
   <h1>{{ reversedMessage }}</h1>
   <h1>{{ reversedMessage }}</h1>
   <h1>{{ reversedMessage }}</h1>
@@ -20,17 +23,33 @@
 export default {
   data() {
     return {
+      // Getter
+      // Setter : 값을 지정하는 용도
       msg: 'Hello Computed!'
     }  
   },
+  // ReadOnly : 읽기전용
   computed: {
-    reversedMessage() {
-      return this.msg.split('').reverse().join('')
+    // Getter : 값을 얻어오는 용도
+    // reversedMessage() {
+    //   return this.msg.split('').reverse().join('')
+    // }
+    // Getter, Setter
+    reversedMessage: {
+      get() {
+        return this.msg.split('').reverse().join('')
+      },
+      // set(들어오는 값)
+      // reversedMessage 값을 할당할때만 동작
+      set(newValue) {
+        // console.log(newValue)로 값을 확인하고 작업!
+        this.msg = newValue
+      }
     }
   },
   methods: {
-    reverseMessage() {
-      return this.msg.split('').reverse().join('')
+    add() {
+      this.reversedMessage += '!?'
     }
   }
 }

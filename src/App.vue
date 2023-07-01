52386f649862ed766b858a0afcbcb48d1451c877
June <sungj1207@gmail.com>
2023-07-01 18:05:27 +0900

    이벤트 핸들링

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,33 @@
 <template>
   <!-- 
-    [v-for] : 반복문
-    - 구분할 수 있는 id와 같은 속성값을 넣어 최적화 시켜주자!!
-    - fruits = 문자열을 가지고 있는 배열 데이터
-    - 배열 데이터 갯수 만큼 li tag를 반복해서 출력하고 하나씩 추출해서 fruit라는 변수에 담아서 활용
-    - :key = data 연결할 수 있음 (li tag 고유하다)
-    - {{ fruit }} = 화면에 출력
+    [Event 객체]
+    - click event가 발생했을 때 event의 정보를 가지고 있는 객체 데이터
+    - ex) PointerEvent
+    - 발생한 event의 실제 데이터를 가지고 있다
+    - $event = event 객체가 나와서 들어간다
    -->
-  <!-- 
-    [배열 데이터 반복 출력]
-    - 반복되는 배열데이터(newFruits)를 매계변수(fruit)에 담아
-    - 속성들을(id, name) 활용하여 화면에 출력
-  -->
-  <button @click="handler">
-    Click me!
+  <button @click="handler('hi', $event)">
+    Click 1
   </button>
-  <ul>
-    <li
-      v-for="{ id, name} in newFruits"
-      :key="id">
-      {{ name }}-{{ id }}
-    </li>
-  </ul>
+  <button @click="handler('what', $event)">
+    Click 2
+  </button>
 </template>
 
 <script>
-// npm i -D shortid
-import shortid from 'shortid'
-
 export default {
-  data() {
-    return {
-      fruits: ['Apple', 'Banana', 'Cherry']
-    }
-  },
-  computed: {
-    newFruits() {
-      return this.fruits.map(fruit => {
-        // 배열 데이터 = 객체 데이터(문자 데이터X)
-        // { id: 0, name: 'Apple'}...
-        return {
-          id: shortid.generate(),
-          name: fruit
-        }
-      })
-    }
-  },
-  // 반응성 push
-  // click me!를 누르면 Orange data 추가
+//   methods: {
+//     // event라는 매개변수 = data 받아주는 역할 = 변경하여 사용 가능
+//     handler(event) {
+//       console.log(event)
+//       console.log(event.target.textContent)
+//     }
+//   }
+// }
   methods: {
-    handler() {
-      this.fruits.push('Orange')
+    handler(msg, event) {
+      console.log(msg) // hi + PointerEvent
+      console.log(event) // what + PointerEvent
     }
   }
 }

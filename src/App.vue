cc79b6a09f97fa1c63bac56fec70d140189e530a
June <sungj1207@gmail.com>
2023-07-01 19:14:32 +0900

    이벤트 수식어

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,38 @@
 <template>
   <!-- 
-    [Event 객체]
-    - click event가 발생했을 때 event의 정보를 가지고 있는 객체 데이터
-    - ex) PointerEvent
-    - 발생한 event의 실제 데이터를 가지고 있다
-    - $event = event 객체가 나와서 들어간다
-   -->
-  <button @click="handler('hi', $event)">
-    Click 1
-  </button>
-  <button @click="handler('what', $event)">
-    Click 2
-  </button>
+    @wheel 동작할 때마다 부하가 걸린다.
+    - .passive 사용
+    - 로직의 처리와 화면과 분리
+  -->
+  <div
+    class="parent"
+    @wheel="handler">
+    <div class="child"></div>
+  </div>
 </template>
 
 <script>
 export default {
-//   methods: {
-//     // event라는 매개변수 = data 받아주는 역할 = 변경하여 사용 가능
-//     handler(event) {
-//       console.log(event)
-//       console.log(event.target.textContent)
-//     }
-//   }
-// }
   methods: {
-    handler(msg, event) {
-      console.log(msg) // hi + PointerEvent
-      console.log(event) // what + PointerEvent
+    handler(event) {
+      console.log(event)
     }
   }
 }
 </script>
+
+<style scoped lang="scss">
+  .parent {
+    width: 200px;
+    height: 100px;
+    background-color: royalblue;
+    margin: 10px;
+    padding: 10px;
+    overflow: auto; // scroll bar
+    .child {
+      width: 100px;
+      height: 2000px;
+      background-color: orange;
+    }
+  }
+</style>
